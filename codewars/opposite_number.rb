63e6e73805b33cabbe26ce6d242c3f1a9b8fbd24
robert haynes <roberthaynes@roberts-MacBook.local>
2018-07-31 12:24:59 -0400

    opposite number

---
+++ codewars/opposite_number.rb
@@ -0,0 +1,11 @@
+# Very simple, given a number, find its opposite. But can you do it in 1 line of code and without any conditionals?
+
+def opposite(number)
+  number * -1
+end
+
+# best practice
+
+def opposite n
+  -n
+end
