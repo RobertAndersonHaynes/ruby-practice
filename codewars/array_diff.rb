171043adc0d81dd4b120b3bb116b62d1bf4e9e0e
robert haynes <roberthaynes@roberts-MacBook.local>
2018-07-31 12:15:28 -0400

    array diff

---
+++ codewars/array_diff.rb
@@ -0,0 +1,11 @@
+# Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+def array_diff(a, b)
+  a - b
+end
+
+# best practice
+
+def array_diff(a, b)
+  a - b
+end
